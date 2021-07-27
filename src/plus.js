d238c259413fed453cabcee8cf1e8981a006b460
Alisha Brodin <alisha.brodin@gmail.com>
2021-07-27 19:43:09 +0200

    cleaned the code and changed the time format

--- src/plus.js
+++ src/plus.js
@@ -1,5 +1,4 @@
 let todaysDate = new Date();
-console.log(todaysDate);
 
 let days = [
   "Sunday",
@@ -29,20 +28,29 @@ let months = [
 handleCurrentTemp();
 
 let day = days[todaysDate.getDay()];
-console.log(day);
-
 let date = todaysDate.getDate();
 let year = todaysDate.getFullYear();
 let month = months[todaysDate.getMonth()];
-let time = todaysDate.toLocaleTimeString();
+let hours = todaysDate.getHours();
+let mints = todaysDate.getMinutes();
+let parameter = "AM";
+if (hours > 12) {
+  hours = hours - 12;
+  hours = `0${hours}`;
+  parameter = "PM";
+} else if (hours === 0) {
+  hours = 12;
+  parameter = "AM";
+}
 
-console.log(time);
-let formatTime = `${time} Hrs`;
+if (mints < 10) {
+  mints = `0${mints}`;
+}
 
+let formatTime = `${hours}:${mints} ${parameter}`;
 let currentDate = `${day}, ${date} ${month} ${year}`;
-console.log(currentDate);
 let updateDate = document.querySelector("#current-date");
-console.log(updateDate);
+
 updateDate.innerHTML = currentDate;
 let updateTime = document.querySelector(".time");
 updateTime.innerHTML = formatTime;
@@ -51,23 +59,17 @@ updateTime.innerHTML = formatTime;
 
 function handlePosition(position) {
   let lat = position.coords.latitude;
-  console.log(lat);
   let lon = position.coords.longitude;
-  console.log(lon);
   let apiKey = "298159ea1285926f2960068f39a80cbf";
   let geolocationAPI = `https://api.openweathermap.org/data/2.5/weather?&units=metric&lat=${lat}&lon=${lon}&appid=${apiKey}`;
   axios.get(geolocationAPI).then(showlocation);
 }
 
 function showlocation(location) {
-  console.log(location);
   let loc = location.data.name;
-  console.log(loc);
   let inputPlace = document.querySelector(".searchbar");
-
   let place = document.querySelector(".place");
   let cityName = inputPlace.value;
-  console.log(cityName);
   place.innerHTML = loc;
 
   let temperature = Math.round(location.data.main.temp);
@@ -75,12 +77,12 @@ function showlocation(location) {
   currentTemp.innerHTML = temperature;
 
   let humidity = location.data.main.humidity;
-  console.log(humidity);
+
   let humid = document.querySelector("#Hvalue");
   humid.innerHTML = ` ${humidity}%`;
 
   let wind = location.data.wind.speed;
-  console.log(wind);
+
   let windy = document.querySelector("#Wvalue");
   windy.innerHTML = ` ${wind}km/hr`;
 
@@ -88,14 +90,11 @@ function showlocation(location) {
   status.innerHTML = location.data.weather[0].main;
 
   let image = document.querySelector("img");
-  console.log("im here displaying the image value");
-  console.log(image);
+
   let icon = location.data.weather[0].icon;
   let link = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-  console.log(link);
-  console.log("icon link");
+
   document.getElementById("weatherimg").src = link;
-  console.log(image.innerHTML);
 }
 
 function handleCurrentTemp() {
@@ -103,6 +102,7 @@ function handleCurrentTemp() {
 }
 
 //Handling the API response and displaying it in the webpage->Temperature, Humidity, Wind<---//
+//handling error//
 
 function handleResponse(response) {
   let temperature = Math.round(response.data.main.temp);
@@ -110,12 +110,12 @@ function handleResponse(response) {
   currentTemp.innerHTML = temperature;
 
   let humidity = response.data.main.humidity;
-  console.log(humidity);
+
   let humid = document.querySelector("#Hvalue");
   humid.innerHTML = ` ${humidity}%`;
 
   let wind = response.data.wind.speed;
-  console.log(wind);
+
   let windy = document.querySelector("#Wvalue");
   windy.innerHTML = ` ${wind}km/hr`;
 
@@ -123,14 +123,11 @@ function handleResponse(response) {
   status.innerHTML = response.data.weather[0].main;
 
   let image = document.querySelector("img");
-  console.log("im here displaying the image value");
-  console.log(image);
+
   let icon = response.data.weather[0].icon;
   let link = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-  console.log(link);
-  console.log("icon link");
+
   document.getElementById("weatherimg").src = link;
-  console.log(image.innerHTML);
 }
 
 //get the value (City name)from the searchbar and display it in the webpage //
@@ -141,7 +138,7 @@ function handlesearch(event) {
 
   let place = document.querySelector(".place");
   let cityName = inputPlace.value;
-  console.log(cityName);
+
   place.innerHTML = cityName;
 
   //Api service call to the weather API//
@@ -149,8 +146,11 @@ function handlesearch(event) {
   let apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric";
   let apiKey = "298159ea1285926f2960068f39a80cbf";
   let apiService = `${apiUrl}&q=${cityName}&appid=${apiKey}`;
-  console.log(apiService);
-  axios.get(`${apiUrl}&q=${cityName}&appid=${apiKey}`).then(handleResponse);
+
+  axios
+    .get(`${apiUrl}&q=${cityName}&appid=${apiKey}`)
+    .then(handleResponse)
+    .catch((error) => alert("Sorry you have entered an incorrect city"));
 
   //reset the searchbar//
   let resetData = document.querySelector("form");
@@ -161,14 +161,13 @@ function handlesearch(event) {
 
 function converttoF() {
   let temp = document.querySelector(".currentTemp");
-  console.log(temp);
+
   temp.innerHTML = "77";
 }
 
 function converttoC() {
   let temp = document.querySelector(".currentTemp");
-  //var temp = document.getElementById("#temp");
-  //console.log(temp);
+
   temp.innerHTML = "25";
 }
 
