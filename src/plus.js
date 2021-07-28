fab757c5416dbff7d0e5a2810380d049cfa31ec7
Alisha Brodin <alisha.brodin@gmail.com>
2021-07-28 19:08:24 +0200

    made changes the display the cityname from the api call.

--- src/plus.js
+++ src/plus.js
@@ -1,61 +1,63 @@
-let todaysDate = new Date();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-let months = [
-  "Jan",
-  "Feb",
-  "Mar",
-  "Apr",
-  "May",
-  "Jun",
-  "Jul",
-  "Aug",
-  "Sep",
-  "Oct",
-  "Nov",
-  "Dec",
-];
-
 handleCurrentTemp();
 
-let day = days[todaysDate.getDay()];
-let date = todaysDate.getDate();
-let year = todaysDate.getFullYear();
-let month = months[todaysDate.getMonth()];
-let hours = todaysDate.getHours();
-let mints = todaysDate.getMinutes();
-let parameter = "AM";
-if (hours > 12) {
-  hours = hours - 12;
-  parameter = "PM";
-} else if (hours === 0) {
-  hours = 12;
-  parameter = "AM";
-}
+function formatdtandTime(apidt) {
+  let todaysDate = new Date(apidt);
 
-if (hours < 10) {
-  hours = `0${hours}`;
-}
-if (mints < 10) {
-  mints = `0${mints}`;
-}
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-let formatTime = `${hours}:${mints} ${parameter}`;
-let currentDate = `${day}, ${date} ${month} ${year}`;
-let updateDate = document.querySelector("#current-date");
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
 
-updateDate.innerHTML = currentDate;
-let updateTime = document.querySelector(".time");
-updateTime.innerHTML = formatTime;
+  let day = days[todaysDate.getDay()];
+  let date = todaysDate.getDate();
+  let year = todaysDate.getFullYear();
+  let month = months[todaysDate.getMonth()];
+  let hours = todaysDate.getHours();
+  let mints = todaysDate.getMinutes();
+  let parameter = "AM";
+  if (hours > 12) {
+    hours = hours - 12;
+    parameter = "PM";
+  } else if (hours === 0) {
+    hours = 12;
+    parameter = "AM";
+  }
+
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  if (mints < 10) {
+    mints = `0${mints}`;
+  }
+
+  let formatTime = `${hours}:${mints} ${parameter}`;
+  let currentDate = `${day}, ${date} ${month} ${year}`;
+  let updateDate = document.querySelector("#current-date");
+
+  updateDate.innerHTML = currentDate;
+  let updateTime = document.querySelector(".time");
+  updateTime.innerHTML = formatTime;
+}
 
 //currentTemp button Handling//
 
@@ -68,10 +70,8 @@ function handlePosition(position) {
 }
 
 function showlocation(location) {
-  let loc = location.data.name;
   let inputPlace = document.querySelector(".searchbar");
   let place = document.querySelector(".place");
-  let cityName = inputPlace.value;
   temperature = Math.round(location.data.main.temp);
   let currentTemp = document.querySelector(".currentTemp");
   let humidity = location.data.main.humidity;
@@ -81,13 +81,16 @@ function showlocation(location) {
   let status = document.querySelector(".status");
   let image = document.querySelector("img");
   let icon = location.data.weather[0].icon;
-  place.innerHTML = loc;
+  place.innerHTML = location.data.name;
   currentTemp.innerHTML = temperature;
   humid.innerHTML = ` ${humidity}%`;
   windy.innerHTML = ` ${wind}km/hr`;
   status.innerHTML = location.data.weather[0].description;
   let link = `http://openweathermap.org/img/wn/${icon}@2x.png`;
-  document.getElementById("#weatherimg").src = link;
+  document.getElementById("weatherimg").src = link;
+  let x = location.data.dt;
+  let y = x * 1000;
+  formatdtandTime(y);
 }
 
 function handleCurrentTemp() {
@@ -98,20 +101,21 @@ function handleCurrentTemp() {
 //handling error//
 
 function handleResponse(response) {
+  let place = document.querySelector(".place");
+  place.innerHTML = response.data.name;
+
   temperature = Math.round(response.data.main.temp);
   let currentTemp = document.querySelector(".currentTemp");
   currentTemp.innerHTML = temperature;
 
-  let humidity = response.data.main.humidity;
   let humid = document.querySelector("#Hvalue");
-  humid.innerHTML = ` ${humidity}%`;
+  humid.innerHTML = ` ${response.data.main.humidity}%`;
 
-  let wind = response.data.wind.speed;
   let windy = document.querySelector("#Wvalue");
-  windy.innerHTML = ` ${wind}km/hr`;
+  windy.innerHTML = ` ${response.data.wind.speed}km/hr`;
 
   let status = document.querySelector(".status");
-  status.innerHTML = response.data.weather[0].main;
+  status.innerHTML = response.data.weather[0].description;
 
   let image = document.querySelector("img");
   let icon = response.data.weather[0].icon;
@@ -127,7 +131,6 @@ function handlesearch(event) {
   let inputPlace = document.querySelector(".searchbar");
   let place = document.querySelector(".place");
   let cityName = inputPlace.value;
-  place.innerHTML = cityName;
 
   //Api service call to the weather API//
 
@@ -136,7 +139,7 @@ function handlesearch(event) {
   let apiService = `${apiUrl}&q=${cityName}&appid=${apiKey}`;
 
   axios
-    .get(`${apiUrl}&q=${cityName}&appid=${apiKey}`)
+    .get(apiService)
     .then(handleResponse)
     .catch((error) => alert("Sorry you have entered an incorrect city"));
 
@@ -149,13 +152,11 @@ function handlesearch(event) {
 
 function converttoF() {
   let temp = document.querySelector(".currentTemp");
-
   temp.innerHTML = Math.round(temperature * 1.8 + 32);
 }
 
 function converttoC() {
   let temp = document.querySelector(".currentTemp");
-
   temp.innerHTML = temperature;
 }
 
